d3e556ef2350cef5a1fba38f56674e8e0fec78c0
Volcaronitee <167378384+Volcaronitee1@users.noreply.github.com>
2024-04-17 19:43:05 -0500

    Update VolcAddons.java

--- forge/src/main/java/com/volca/VolcAddons/VolcAddons.java
+++ forge/src/main/java/com/volca/VolcAddons/VolcAddons.java
@@ -3,7 +3,7 @@ package com.volca.VolcAddons;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser; 
 import net.minecraft.client.Minecraft;
-import net.minecraft.event.ClickEvent;
+import net.minecraft.event.ClickEvent; 
 import net.minecraft.util.ChatStyle;
 import net.minecraft.util.EnumChatFormatting;
 import net.minecraftforge.client.ClientCommandHandler;
